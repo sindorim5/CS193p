9c57031c469f59f02aa837281337cb4e2c8f8b93
Kihun SONG <87517193+sindorim5@users.noreply.github.com>
2022-01-25 17:10:41 +0900

    Assignment1

---
+++ Assignment/assignment1/assignment1App.swift
@@ -0,0 +1,17 @@
+//
+//  assignment1App.swift
+//  assignment1
+//
+//  Created by Kihun SONG on 2022/01/22.
+//
+
+import SwiftUI
+
+@main
+struct assignment1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
