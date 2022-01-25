5a7ac278a122b6a987d6c759439d33d1ead762e9
Kihun SONG <87517193+sindorim5@users.noreply.github.com>
2022-01-25 17:22:23 +0900

    Lecture 1, 2

---
+++ LectureFollowUpCode/Lecture 1, 2/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Kihun SONG on 2022/01/25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
